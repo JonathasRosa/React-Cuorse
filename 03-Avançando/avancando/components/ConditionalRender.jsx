9eb55ec900c10227c78503f00ffbf8ec7564e31b
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-07-28 16:07:19 -0300

    Update Course React

---
+++ 03-Avançando/avancando/components/ConditionalRender.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const ConditionalRender = () => {
+    const [x] = useState(false);
+    const [name, setName] = useState("Carlos")
+    return (
+        <div>
+            <h1>Isso será exibído?</h1>
+            {x && <p>Se x for true, sim!</p>}
+            {!x && <p>Agora x é Falso</p>}
+            <h1>If Ternário</h1>
+            {name === "Jonathas" ? (
+                <div>
+                    <p>O nome é Jonathas</p>
+                </div>
+            ) : (
+                <di>
+                    <p>Nome não encontrado</p>
+                </di>
+            )}
+            <button onClick={() => setName("Jonathas")}>Clica aqui!!!</button>
+        </div>
+    )
+};
+
+export default ConditionalRender;
