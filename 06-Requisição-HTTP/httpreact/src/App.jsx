e5b04903b176d110ffcdec80f7e8648086c15d18
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-08-14 16:14:32 -0300

    Update Curso de React

--- 06-Requisição-HTTP/httpreact/src/App.jsx
+++ 06-Requisição-HTTP/httpreact/src/App.jsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import './App.css';
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ products, setProducts ] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  //1 - Resgatando dados
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    async function fetchData(){
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  //2 - ADD de products
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const product = {
+      name,
+      price
+    };
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    });
+  };
+  // 3 - Carregamento dinamico - entender melhor para aplicação.
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) =>(
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
+      <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome: 
+              <input 
+              type='text' 
+              value={name} 
+              name='name'
+              onChange={(e) => setName(e.target.value)} />
+            </label>
+            <label>
+              Preço: 
+              <input 
+              type='number' 
+              value={price} 
+              name='price'
+              onChange={(e) => setPrice(e.target.value)} />
+            </label>
+            <button type='submit' value="criar">Criar</button>
+          </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
